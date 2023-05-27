53d0a69b27d2f3f7ed82ec56ad4623811ca3a4bb
SUJISHNA V P <110602045+sujishna@users.noreply.github.com>
2023-05-28 00:40:35 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,13 @@
+function btnclick(val){
+    console.log(val)
+    document.getElementById("screen").value=document.getElementById("screen").value+val
+    
+}
+function sclear(){
+    document.getElementById("screen").value=""
+}
+function equalclick(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById('screen').value=result
+}
